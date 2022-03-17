b860118d42c5c5e9135a108c76c437c05c225f26
Teo Fumagalli <teo.fum@gmail.com>
2022-03-17 19:29:53 -0300

    Fancy wt card animation

--- components/index/Walkthrough.tsx
+++ components/index/Walkthrough.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from 'react';
+
 import Image from 'next/image';
 import cn from 'classnames';
 
@@ -6,6 +8,28 @@ import typeStyles from '../../styles/Type.module.scss';
 import utilStyles from '../../styles/Utils.module.css';
 
 const Walkthrough = () => {
+  const [showCards, setShowCards] = useState(false);
+  let card: HTMLElement | null = null;
+
+  useEffect(() => {
+    if (showCards) return;
+    const el = card;
+
+    const handler = () => {
+      const rect = el?.getBoundingClientRect();
+      const threshold = window.innerHeight - (rect?.height || 0) / 2;
+      if ((rect?.top || 0) < threshold) {
+        setShowCards(true);
+        window.removeEventListener('scroll', handler);
+      }
+    };
+
+    window.addEventListener('scroll', handler);
+    handler(); // Call once to show cards if they're already in viewport
+
+    return () => window.removeEventListener('scroll', handler);
+  }, []);
+
   const cardTitles = [
     '1—Browse',
     '2—Choose',
@@ -25,8 +49,14 @@ const Walkthrough = () => {
         srcSet='/illustrations/hero-responsive.png 1x, /illustrations/hero-desktop.png 2x' />
 
       {[0, 1, 2].map(i => (
-        <div key={i} className={cn(styles.card, utilStyles.elevation2)}
-          style={{ '--n': i - 1 } as any}>
+        <div key={i}
+          className={cn(
+            styles.card,
+            utilStyles.elevation2,
+            { [styles.visible]: showCards }
+          )}
+          style={{ '--n': i - 1 } as any}
+          ref={el => { if (i === 1) card = el; }}>
           <div className={styles.cardTop}>
             <Image src={`/illustrations/wt-${i + 1}-desktop.png`}
               width={508} height={498} />
